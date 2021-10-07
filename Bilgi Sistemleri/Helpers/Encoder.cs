c9720e04c5c8799f536b83eed3ae230edacf71ac
berkayersoy <berkay.ersoy.007@gmail.com>
2021-10-07 10:44:04 +0300

    created

---
+++ Bilgi Sistemleri/Helpers/Encoder.cs
@@ -0,0 +1,34 @@
+﻿namespace Bilgi_Sistemleri.Helpers
+{
+    public class Encoder
+    {
+        public static char Cipher(char ch, int key)
+        {
+            if (!char.IsLetter(ch))
+            {
+
+                return ch;
+            }
+
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + key) - d) % 26) + d);
+
+
+        }
+        public static string Decipher(string input, int key)
+        {
+            return Encipher(input, 26 - key);
+        }
+
+        public static string Encipher(string input, int key)
+        {
+            string output = string.Empty;
+
+            foreach (char ch in input)
+                output += Cipher(ch, key);
+
+            return output;
+        }
+    }
+
+}
---
+++ Bilgi Sistemleri/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Bilgi_Sistemleri.Helpers;
+
+namespace Bilgi_Sistemleri
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Type a string to encrypt:");
+            string UserString = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            Console.Write("Enter your Key");
+            int key = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+
+
+            Console.WriteLine("Encrypted Data");
+
+            string cipherText = Encoder.Encipher(UserString, key);
+            Console.WriteLine(cipherText);
+            Console.Write("\n");
+
+            Console.WriteLine("Decrypted Data:");
+
+            string t = Encoder.Decipher(cipherText, key);
+            Console.WriteLine(t);
+            Console.Write("\n");
+
+            string val = "abc";
+            string encrypt = "";
+            for (int iChar = 0; iChar < val.Length; iChar++)
+            {
+                encrypt += (char) (val[iChar] + 3);
+            }
+
+            string decrypt = "";
+            for (int iChar = 0; iChar < encrypt.Length; iChar++)
+            {
+                decrypt += (char) (encrypt[iChar] - 3);
+            }
+        }
+    }
+}
