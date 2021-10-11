ed80d6bd9ae9548c8803bfc1f9ab7b0f42617521
berkayersoy <berkay.ersoy.007@gmail.com>
2021-10-11 18:28:21 +0300

    small changes

--- Bilgi Sistemleri/Helpers/Encoder.cs
+++ Bilgi Sistemleri/Helpers/Encoder.cs
@@ -1,33 +1,43 @@
-﻿namespace Bilgi_Sistemleri.Helpers
+﻿using System;
+using System.Linq;
+
+namespace Bilgi_Sistemleri.Helpers
 {
     public class Encoder
     {
-        public static char Cipher(char ch, int key)
+        private static char[] _alphabet = new char[]
         {
-            if (!char.IsLetter(ch))
+            'A', 'B', 'C','Ç' , 'D', 'E', 'F', 'G','Ğ' ,'H', 'I', 'İ', 'J', 'K', 'L', 'M', 'N', 'O', 'Ö' ,
+            'P', 'Q', 'R', 'S','Ş' , 'T', 'U', 'Ü', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'ç','d', 'e', 'f', 'g','ğ', 'h', 'ı','i', 'j', 'k', 'l', 'm', 'n', 'o','ö',
+            'p', 'q', 'r', 's','ş', 't', 'u','ü', 'v', 'w', 'x', 'y', 'z',
+            '1','2','3','4','5','6','7','8','9','0',' ','`','~','!','\'','^','#','+','$','%','½','&','/','{','(','[',')',']','=','}','?','*','\\','-','_',',','.',';','.',':','<','>','|','@'
+        };
+        public static string Encipher(string text, int key)
+        {
+            string encodedMessage = "";
+            for (int i = 0; i < text.Length; i++)
             {
-
-                return ch;
+                var charIndex = Array.IndexOf(_alphabet, text[i]);
+                if (charIndex + key > _alphabet.Length - 1)
+                {
+                    int shift = (charIndex + key) % _alphabet.Length;
+                    encodedMessage += _alphabet[shift];
+                }
+                else
+                {
+                    int shift = charIndex + key;
+                    encodedMessage += _alphabet[shift];
+                }
             }
 
-            char d = char.IsUpper(ch) ? 'A' : 'a';
-            return (char)((((ch + key) - d) % 26) + d);
+            return encodedMessage;
 
 
         }
         public static string Decipher(string input, int key)
         {
-            return Encipher(input, 26 - key);
-        }
-
-        public static string Encipher(string input, int key)
-        {
-            string output = string.Empty;
-
-            foreach (char ch in input)
-                output += Cipher(ch, key);
-
-            return output;
+            return Encipher(input, _alphabet.Length - key);
         }
     }
 
---
+++ Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Bilgi_Sistemleri.Helpers;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "abcd ABCD 1234567890CCC           ";
+            var newText = Encoder.Encipher(text, Constants.Key);
+            var decodedText = Encoder.Decipher(newText, Constants.Key);
+            Console.WriteLine(newText);
+            Console.WriteLine(decodedText);
+        }
+    }
+}
